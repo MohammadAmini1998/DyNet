6485bef117eb736ae4b27557e65746d3cb39806b
Mohammad Amini <49214384+MohammadAmini1998@users.noreply.github.com>
2023-08-04 01:27:02 +0330

    Full communication and without communication

---
+++ envs/config_env.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding=utf8
+
+
+def config_env(_flags):
+    flags = _flags
+
+    # Scenario
+    flags.DEFINE_string("scenario", "predator_prey_obs", "Scenario")
+    flags.DEFINE_integer("n_predator", 4, "Number of predators")
+    flags.DEFINE_integer("n_prey", 1, "Number of preys")
+    flags.DEFINE_boolean("obs_diagonal", True, "Whether the agent can see in diagonal directions")
+    flags.DEFINE_boolean("moving_prey", True, "Whether the prey is moving")
+    flags.DEFINE_integer("obs_range", 1, "Observation range")
+    flags.DEFINE_integer("hetero", 1, "Heterogeneity of observation range")
+
+    # Observation
+    flags.DEFINE_integer("history_len", 1, "How many previous steps we look back")
+
+    # core
+    flags.DEFINE_integer("map_size", 7, "Size of the map")
+    flags.DEFINE_float("render_every", 1000, "Render the nth episode")
+
+    # GUI
+    flags.DEFINE_boolean("gui", False, "Activate GUI")
+
+
+def get_filename():
+    import config
+    FLAGS = config.flags.FLAGS
+
+    return "s-"+FLAGS.scenario+"-map-"+str(FLAGS.map_size)+"-or-"+str(FLAGS.obs_range)
---
+++ main.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding=utf8
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+import logging
+import make_env
+import agents
+import config
+import time
+import random
+import tensorflow as tf
+import numpy as np
+
+FLAGS = config.flags.FLAGS
+
+def set_seed(seed):
+
+    random.seed(seed)
+    np.random.seed(seed)
+    tf.compat.v1.set_random_seed(seed)
+    return None
+
+
+if __name__ == '__main__':
+
+    set_seed(1)
+
+    logger_env = logging.getLogger('GridMARL')
+    logger_agent = logging.getLogger('Agent')
+
+    env = make_env.make_env(FLAGS.scenario)
+    logger_env.info('GridMARL Start with %d predator(s) and %d prey(s)', FLAGS.n_predator, FLAGS.n_prey)
+
+    logger_agent.info('Agent: {}'.format(FLAGS.agent))
+    trainer = agents.load(FLAGS.agent+"/trainer.py").Trainer(env)
+
+    print(FLAGS.agent, config.file_name)
+
+    if FLAGS.train:
+        start_time = time.time()
+        trainer.learn()
+        finish_time = time.time()
+        trainer.test()
+        print("TRAINING TIME (sec)", finish_time - start_time)
+    else:
+        trainer.test()
+
+
+    print("LOG_FILE:\t" + config.log_filename)
